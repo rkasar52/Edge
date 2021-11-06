a5b34efdee67e07db8142a4abdee4f4e88967d30
James <jhenn@mit.edu>
2021-11-06 12:42:17 -0400

    add function to plot availability by TOD

--- edge_eda.R
+++ edge_eda.R
@@ -1,10 +1,21 @@
 library(tidyverse)
 library(ggplot2)
 library(lubridate)
+library(zoo)
 
 
 bluebike <- read.csv("202109-bluebikes-tripdata.csv")
+stations <- read.csv("current_bluebikes_stations.csv")
 bluebike$start.station.id <- as.factor(bluebike$start.station.id)
+
+prejoin <- stations %>% 
+  select(Name, Total.docks)
+
+joinstation <- bluebike %>% 
+  left_join(prejoin, by=c("start.station.name" = "Name")) %>% 
+  rename(start.station.docks = Total.docks)
+  
+
 
 # time zone is UTC -- we will convert to EDT
 bluebike$starttime <- as.POSIXct(bluebike$starttime)
@@ -49,7 +60,45 @@ top10stations <- stations_sep2021 %>%
   filter(rank <= 10) %>% 
   left_join()
 
+capacityDay_start <- function(data, station, day){
+
+  station.capacity <- stations$Total.docks[which(stations$Name == station)]
   
+  stationDay <- data %>% 
+    filter(tripduration <= 86400 & (start.station.name == station | end.station.name == station) & (substr(starttime_edt, 1, 10) == day | substr(stoptime_edt, 1, 10) == day))
   
+  start <- stationDay %>% 
+    filter(start.station.name == station) %>% 
+    select(starttime_edt) %>% 
+    rename(time = starttime_edt)
   
-ggplot(top10stations)
+  stop <- stationDay %>% 
+    filter(end.station.name == station) %>% 
+    select(stoptime_edt) %>% 
+    rename(time = stoptime_edt)
+  
+  start$capacity <- -1
+  stop$capacity <- 1
+  
+  together <- rbind(start, stop) %>% 
+    arrange(time)
+  together$trans <- 1:nrow(together)
+  together$currentcapac <- cumsum(together$capacity)
+  together$available <- station.capacity + together$currentcapac
+  
+  #currentCapacity <- together %>% 
+  #  filter(trans <= transNum) %>% 
+  #  summarise(avail = sum(capacity))
+  
+  #return(currentCapacity[1,1])
+  return(together)
+}
+
+
+AmesJan03_trans <- capacityDay_start(data, "1200 Beacon St", "2021-09-04") 
+
+ggplot(AmesJan03_trans) + geom_line(aes(x=time, y=available))
+
+
+stationDay <- data %>% 
+  filter((start.station.name == "1200 Beacon St" | end.station.name == "1200 Beacon St") & (substr(starttime_edt, 1, 10) == "2021-09-04" | substr(stoptime_edt, 1, 10) == "2021-09-04"))
